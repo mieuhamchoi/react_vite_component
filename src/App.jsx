2f825984c589cfe89e92fe1e98a9e2b23f211575
Mieu Teacher <mieuteacher@gmail.com>
2023-08-21 20:54:33 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar1 from './components/Navbar'
+
+function App() {
+
+  return (
+    <div>
+      File App Nè
+      {/* <Navbar1></Navbar1> */}
+      <Navbar1/>
+    </div>
+  )
+}
+
+export default App
